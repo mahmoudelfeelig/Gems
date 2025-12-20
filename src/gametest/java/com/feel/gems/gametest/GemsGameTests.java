1fdc3093f1ab03a45b532f6b756844b1cf804811
mahmoudelfeelig <mahmoudelfeelig@gmail.com>
2025-12-20 20:31:18 +0100

    fixed a build issue

--- src/gametest/java/com/feel/gems/gametest/GemsGameTests.java
+++ src/gametest/java/com/feel/gems/gametest/GemsGameTests.java
@@ -11,6 +11,7 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.test.GameTest;
 import net.minecraft.test.TestContext;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
 import net.minecraft.world.GameMode;
@@ -19,19 +20,20 @@ public final class GemsGameTests {
     @GameTest(templateName = FabricGameTest.EMPTY_STRUCTURE, tickLimit = 200)
     public void astralCameraReturnsToStart(TestContext context) {
         ServerWorld world = context.getWorld();
-        ServerPlayerEntity player = context.createMockPlayer(GameMode.SURVIVAL);
+        ServerPlayerEntity player = context.createMockCreativeServerPlayerInWorld();
+        player.changeGameMode(GameMode.SURVIVAL);
 
-        BlockPos start = context.getAbsolute(new BlockPos(0, 2, 0));
-        player.teleport(world, start.getX() + 0.5D, start.getY(), start.getZ() + 0.5D, 0.0F, 0.0F);
+        Vec3d startPos = context.getAbsolute(new Vec3d(0.5D, 2.0D, 0.5D));
+        BlockPos startBlock = BlockPos.ofFloored(startPos);
+        player.teleport(world, startPos.x, startPos.y, startPos.z, 0.0F, 0.0F);
 
         AbilityRuntime.startAstralCamera(player, 20);
 
-        BlockPos moved = start.add(6, 0, 0);
-        player.teleport(world, moved.getX() + 0.5D, moved.getY(), moved.getZ() + 0.5D, 90.0F, 0.0F);
+        Vec3d movedPos = context.getAbsolute(new Vec3d(6.5D, 2.0D, 0.5D));
+        player.teleport(world, movedPos.x, movedPos.y, movedPos.z, 90.0F, 0.0F);
 
-        long checkTick = world.getTime() + 60L;
-        context.runAtTick(checkTick, () -> {
-            if (!player.getBlockPos().equals(start)) {
+        context.runAtTick(80L, () -> {
+            if (!player.getBlockPos().equals(startBlock)) {
                 context.throwGameTestException("Astral Camera did not return player to start position");
             }
             if (player.interactionManager.getGameMode() != GameMode.SURVIVAL) {
@@ -44,17 +46,19 @@ public final class GemsGameTests {
     @GameTest(templateName = FabricGameTest.EMPTY_STRUCTURE, tickLimit = 200)
     public void fluxBeamConsumesCharge(TestContext context) {
         ServerWorld world = context.getWorld();
-        ServerPlayerEntity player = context.createMockPlayer(GameMode.SURVIVAL);
+        ServerPlayerEntity player = context.createMockCreativeServerPlayerInWorld();
+        player.changeGameMode(GameMode.SURVIVAL);
 
-        BlockPos start = context.getAbsolute(new BlockPos(0, 2, 0));
-        player.teleport(world, start.getX() + 0.5D, start.getY(), start.getZ() + 0.5D, 0.0F, 0.0F);
+        Vec3d startPos = context.getAbsolute(new Vec3d(0.5D, 2.0D, 0.5D));
+        player.teleport(world, startPos.x, startPos.y, startPos.z, 0.0F, 0.0F);
+        player.setYaw(0.0F);
+        player.setPitch(0.0F);
 
         // Place a target in front of the player (yaw 0 faces +Z).
-        context.spawnEntity(EntityType.COW, 0.5F, 2.0F, 5.5F);
+        context.spawnEntity(EntityType.COW, 0.5F, 3.0F, 5.5F);
         FluxCharge.set(player, 100);
 
-        long runTick = world.getTime() + 2L;
-        context.runAtTick(runTick, () -> {
+        context.runAtTick(2L, () -> {
             boolean activated = new FluxBeamAbility().activate(player);
             if (!activated) {
                 context.throwGameTestException("Flux Beam did not activate (target not acquired)");
@@ -69,18 +73,19 @@ public final class GemsGameTests {
     @GameTest(templateName = FabricGameTest.EMPTY_STRUCTURE, tickLimit = 200)
     public void gemsDoNotDropOnDeath(TestContext context) {
         ServerWorld world = context.getWorld();
-        ServerPlayerEntity player = context.createMockPlayer(GameMode.SURVIVAL);
+        ServerPlayerEntity player = context.createMockCreativeServerPlayerInWorld();
+        player.changeGameMode(GameMode.SURVIVAL);
 
-        BlockPos start = context.getAbsolute(new BlockPos(0, 2, 0));
-        player.teleport(world, start.getX() + 0.5D, start.getY(), start.getZ() + 0.5D, 0.0F, 0.0F);
+        Vec3d startPos = context.getAbsolute(new Vec3d(0.5D, 2.0D, 0.5D));
+        BlockPos startBlock = BlockPos.ofFloored(startPos);
+        player.teleport(world, startPos.x, startPos.y, startPos.z, 0.0F, 0.0F);
 
         player.giveItemStack(new ItemStack(ModItems.ASTRA_GEM));
 
         player.damage(player.getDamageSources().generic(), 10_000.0F);
 
-        long checkTick = world.getTime() + 2L;
-        context.runAtTick(checkTick, () -> {
-            Box box = new Box(start).expand(12.0D);
+        context.runAtTick(5L, () -> {
+            Box box = new Box(startBlock).expand(12.0D);
             boolean foundGemDrop = !world.getEntitiesByType(EntityType.ITEM, box, item -> item.getStack().isOf(ModItems.ASTRA_GEM)).isEmpty();
 
             if (foundGemDrop) {
